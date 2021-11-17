7a345cd27fd130e9158b72506022beb65787c9fd
Gustav Bäckman <a19gusba@student.his.se>
2021-11-17 15:21:17 +0100

    climatecode data from api

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-function App() {
+function App({ codes }) {
+  console.log(codes)
 
   function getCityData() {
     var data = { info: { cityName: "City", country: "Sweden", geodata: "asd", type: "type", zipCode: "12345", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, atque!" }, climateCode: { code: "Af", name: "Tropical rainforest climate Tropical Rainforest", color: "#960000" } }
